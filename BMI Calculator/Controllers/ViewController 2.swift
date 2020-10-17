de529b014d6286d8d9340e96246b4de517765648
1106137105 <1106137105@nkust.edu.tw>
2020-10-17 20:45:10 +0800

    first commit

---
+++ BMI Calculator/Controllers/ViewController 2.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var heightLable: UILabel!
+    @IBOutlet weak var weightLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+}
+
---
+++ BMI.swift
@@ -0,0 +1,18 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by 溫家豪 on 2020/10/17.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value : Float
+    let advice : String
+    let color : UIColor
+    
+    }
+
+
